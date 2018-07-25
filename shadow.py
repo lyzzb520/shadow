204ecffa4c0fffb708ca77ec4591d10fc2296388
lyzzb520 <lyzzb.520@163.com>
2018-07-25 19:09:34 +0800

    fixed bugs

--- shadow.py
+++ shadow.py
@@ -1,10 +1,16 @@
 from appConfig.App import app, db
 import os
+
 dbDir = os.path.dirname(__file__)
+# staticFolder = '%sstatic' % os.sep
 dbDir = '%s%sappConfig%smine.db' % (dbDir, os.sep, os.sep)
+
 if not os.path.exists(dbDir):
     # 创建数据库并创建表
     db.create_all()
+
+
+# app._static_folder = staticFolder
 
 
 @app.route('/')
